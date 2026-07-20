812daed4d9f7faf354a2a4e6f6988e152064a8c3
sunayana1993 <sunayanabhowmik1993@gmail.com>
2026-07-20 18:41:22 +0530

    second commit 2

--- 02_virtual/pyth_id.py
+++ 02_virtual/pyth_id.py
@@ -2,3 +2,9 @@ sum_max=2
 print(f"id of sum_max:{id(sum_max)}")
 sum_max=12
 print(f"id of sum_max:{id(sum_max)}")
+
+spice_mix=set()
+print(f"Initial spice mix id:{id(spice_mix)}")
+spice_mix.add("ginger")
+spice_mix.add("cardamom")
+print(f"Initial spice mix id:{id(spice_mix)}")
