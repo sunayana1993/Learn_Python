da652604838c12e6aa17a26ea9c3a078de292916
sunayana1993 <sunayanabhowmik1993@gmail.com>
2026-07-22 16:56:11 +0530

    tuples in puthon ()

---
+++ 04_data_structure/datastructure_tuple.py
@@ -0,0 +1,10 @@
+#Tuples comes with ()
+#Tuples are immutable
+masala_spices=("cardamom","ginger","cinnamon")
+(spice1,spice2,spice3)=masala_spices
+print(f"spices are {spice1},{spice2},{spice3}")
+
+cardamom,ginger=2,1
+print(cardamom,ginger)
+cardamom,ginger=ginger,cardamom
+print(cardamom,ginger)
