1246a0401c2a33a5a4ced4bace7b27ca067b1f40
sunayana1993 <sunayanabhowmik1993@gmail.com>
2026-07-22 14:05:50 +0530

    string slicing, string encode & decode

--- 03_data_type/datatype_string.py
+++ 03_data_type/datatype_string.py
@@ -1,3 +1,21 @@
 str1="Sunayana"
 str2="chai"
 print(f"{str1} loves {str2}")
+
+#we will also learn slicing of the string
+
+#core,indexing,slicing
+#strings are immutable
+chai_description="Aromatic and Bold"
+print(chai_description[0:7])
+print(chai_description[0:7:2])
+print(chai_description[:7])
+print(f"Last word: {chai_description[12:]}")
+print(f"reverse: {chai_description[::-1]}")
+
+#using special character
+lable_text="chai spécial"
+encoded_label=lable_text.encode("utf-8")
+print(f"encoded text {encoded_label}")
+decoded_label=encoded_label.decode("utf-8")
+print(f"decoded text {decoded_label}")
