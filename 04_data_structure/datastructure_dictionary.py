c687c4e34609a046baafcbcc427583201a347c01
sunayana1993 <sunayanabhowmik1993@gmail.com>
2026-07-24 20:06:29 +0530

    dictionary

---
+++ 04_data_structure/datastructure_dictionary.py
@@ -0,0 +1,12 @@
+#In dictionary order doesn't matter
+#It has key:value format
+dict_label=dict(chai="kadak",type="milk based",sugar=2)
+print(f"chai type is {dict_label}")
+
+chai_recipe={}
+chai_recipe["base"]="black tea"
+chai_recipe["liquid"]="milk"
+
+print(f"chai recipe is {chai_recipe}")
+print(f"chai recipe base is {chai_recipe["base"]}")
+print(f"chai recipe bas:{chai_recipe["liquid"]}")
