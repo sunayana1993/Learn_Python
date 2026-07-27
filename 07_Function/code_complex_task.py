dd5c20e02235845648ac10b34c3262430153fc92
sunayana1993 <sunayanabhowmik1993@gmail.com>
2026-07-28 01:46:01 +0530

    function 1

---
+++ 07_Function/code_complex_task.py
@@ -0,0 +1,17 @@
+def fetch_sales():
+    print("fetching the sales data")
+
+def filter_valid_status():
+    print("Filter valid sales data")
+
+def summarize_data():
+    print("summarizing sales data")
+
+def generate_report():
+    fetch_sales()
+    filter_valid_status()
+    summarize_data()
+    print("Report is ready")
+
+generate_report()
+
---
+++ 07_Function/code_duplication.py
@@ -0,0 +1,5 @@
+def print_order(name,chai_type):
+    print(f"{name} ordered {chai_type} chai")
+
+print_order("Ram","ginger")
+print_order("Meera","Masala")
