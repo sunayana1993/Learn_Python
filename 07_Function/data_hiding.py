f5cd1197d9b400b9aeff88f67794a024e456258d
sunayana1993 <sunayanabhowmik1993@gmail.com>
2026-07-28 12:22:24 +0530

    data hiding

---
+++ 07_Function/data_hiding.py
@@ -0,0 +1,16 @@
+def get_input():
+    print("user will enter the input")
+
+def validate_input():
+    print("Validate the entered input")
+
+def save_to_db():
+    print("save to db")
+
+def register_user():
+    get_input()
+    validate_input()
+    save_to_db()
+    print("user is registered")
+
+register_user()
