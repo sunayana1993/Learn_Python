f68892763d511fd159b60b2ff747419261caec52
sunayana1993 <sunayanabhowmik1993@gmail.com>
2026-07-29 11:53:35 +0530

    star and double star

---
+++ 07_Function/input_parameter.py
@@ -0,0 +1,21 @@
+chai_type="Ginger"
+def prepare_chai(chai_type):
+    print("Preparing ",chai_type)
+
+prepare_chai(chai_type)
+print(chai_type)
+
+chai=[1,2,3]
+
+def edit_chai(cup):
+    chai[1]=42
+
+edit_chai(chai)
+print(chai)
+
+def special_chai(*ingredients,**masala):
+    print("Ingredients are :",ingredients)
+    print("Masala are :",masala)
+
+special_chai("milk","water",masala1="ginger",masala2="lemon")
+
