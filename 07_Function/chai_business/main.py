37aff84c9699c8573f223856f5ef4f4b124775e4
sunayana1993 <sunayanabhowmik1993@gmail.com>
2026-07-30 15:16:54 +0530

    list comprehension and comprehension

--- 07_Function/chai_business/main.py
+++ 07_Function/chai_business/main.py
@@ -5,3 +5,5 @@
 from flavor.flavor import elaichi_chai
 
 print(elaichi_chai())
+
+# __init__.py turns folder into python package
---
+++ 08_comprehensions/list_comprehension.py
@@ -0,0 +1,8 @@
+menu=["Iced Tea",
+      "Green Tea",
+      "Ginger Tea",
+      "Elaichi ea"]
+list_comp=[tea for tea in menu if "Tea" in tea]
+print(list_comp)
+
+#you can't write my_tea for tea in ment, my_tea comes from tea
