cfe41f18a34fe911c14598c9a29534021bf34501
sunayana1993 <sunayanabhowmik1993@gmail.com>
2026-07-30 12:29:51 +0530

    import package

---
+++ 07_Function/chai_business/flavor/flavor.py
@@ -0,0 +1,5 @@
+def elaichi_chai():
+    return "elaichi_chai"
+
+def ginger_chai():
+    return "ginger chai"
---
+++ 07_Function/chai_business/main.py
@@ -0,0 +1,7 @@
+#import flavor.flavor
+
+#print(flavor.flavor.elaichi_chai())
+
+from flavor.flavor import elaichi_chai
+
+print(elaichi_chai())
