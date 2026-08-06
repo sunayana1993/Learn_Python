dfcf97aaf1724c2651a19e77cfceee2f3f2864cd
sunayana1993 <sunayanabhowmik1993@gmail.com>
2026-08-06 15:31:14 +0530

    MRO

---
+++ 10_OOPS/methodresolutionorder.py
@@ -0,0 +1,14 @@
+class A:
+    label="label A:returning from A"
+
+class B(A):
+    label="label B:returning from B"
+
+class C(B):
+    label="label C:returning from C"
+
+class D(B,A):
+    pass
+
+cup=D()
+print(cup.label)
